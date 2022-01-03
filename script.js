e3c3ad131567594dac3120861ed28808cf99027b
Glauber Santana <glauber.silva14@gmail.com>
2022-01-03 18:09:41 -0300

    Fixing typo on word array

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["aos pumões", "ao coração", "ao cerébro", "a vida"],
+        strings: ["aos pulmões", "ao coração", "ao cerébro", "a vida"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
